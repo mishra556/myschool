570a77beee1fac44ac821cc650e0ae9fc5811dfc
mukesh mishra <mishramukesh556@gmail.com>
2020-10-08 22:55:15 +0400

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var fs = require('fs');
 var upload = require('express-fileupload');
 const { request } = require("https");
 
@@ -19,13 +19,14 @@ internetAvailable().then(function () {
    console.log("Internet available");
 }).catch(function () {
    console.log("No internet");
-}); 
+});
 
 
 
 
 var users = [];
 var left = [];
+var client = 0;
 
 io.on('connection', (socket) => {
 
@@ -35,26 +36,34 @@ io.on('connection', (socket) => {
 
 
 
-   socket.on('new-user-join', name => {
+   socket.on('new-user-join', data => {
 
-      socket.emit('message', 'welcome' + ':' + name);
-      socket.emit('heading', 'welcome' + ':' + name);
-      io.emit('auser', name);
-      
-
-      console.log(name + ':' + 'connected');
-
-      
+      socket.emit('message', 'welcome' + ':' + data.name);
+      socket.emit('heading', 'welcome' + ':' + data.name);
 
 
+      io.emit('auser', data.name);
+
+      client++;
+      console.log(data.name + ':' + client + ':' + 'connected');
 
 
-      users[name] = socket.id;
-      left[socket.id] = name;
 
-      io.emit('user-join', name);
+
+      users[data.name] = socket.id;
+      left[socket.id] = data.name;
+
+      io.emit('user-join', data.name);
 
    });
+
+
+
+
+
+
+
+
 
    socket.on('chatm', data => {
       var socketid = users[data.receiver];
@@ -85,95 +94,106 @@ io.on('connection', (socket) => {
    socket.on('upload', name => {
 
 
-          
-      
-      app.post('/' , (req ,res) => {
-         
-         if (req.files){
+
+
+      app.post('/', (req, res) => {
+
+         if (req.files) {
             console.log(req.files);
-            var file=req.files.file;
+            var file = req.files.file;
             console.log(file);
-            var filename=file.name;
+            var filename = file.name;
             console.log(filename);
-            
-            file.mv('./public/img/' + filename , function (err){
-               if(err){
+
+            file.mv('./public/img/' + filename, function (err) {
+               if (err) {
                   res.send(err);
                }
-               else{
-                 
-                 
-                   io.emit('ufname', {name,filename});
-                   
+               else {
+
+
+                  io.emit('ufname', { name, filename });
+
                }
             });
          }
-         
-         
-         
-         });
-         
-      
-      
-      
-      
-      
-      
-      
-         } );
+
+
+
+      });
 
 
 
 
-         socket.on('touser', data=> {
 
-            var socketid = users[data.receiver];
-            send=data.sender;
-            
-            app.post('/' , (req ,res) => {
-               
-               if (req.files){
-                  console.log(req.files);
-                  var file=req.files.file;
-                  console.log(file);
-                  var filename=file.name;
-                  console.log(filename);
-                  
-                  file.mv('./public/img/' + filename , function (err){
-                     if(err){
-                        res.send(err);
-                     }
 
-                     else{
-                       
-                       
-                        io.to(socketid).emit('upname', {send,filename});
-                     }
-                  });
+
+
+   });
+
+
+
+
+   socket.on('touser', data => {
+
+      var socketid = users[data.receiver];
+      send = data.sender;
+
+      app.post('/', (req, res) => {
+
+         if (req.files) {
+            console.log(req.files);
+            var file = req.files.file;
+            console.log(file);
+            var filename = file.name;
+            console.log(filename);
+
+            file.mv('./public/img/' + filename, function (err) {
+               if (err) {
+                  res.send(err);
                }
-               
-               
-               
-               });
-               
-            
-            
-            
-            
-            
-            
-            
-               } );
+
+               else {
 
 
-               socket.on('stream', (image)=>{
-                socket.broadcast.emit('recimg',image);
-               })
-               
-                 
-              
-         });
-           
+                  io.to(socketid).emit('upname', { send, filename });
+               }
+            });
+         }
+
+
+
+
+
+
+
+
+      });
+
+
+
+
+
+
+
+
+   });
+
+
+   socket.on('stream', (image) => {
+
+
+      socket.broadcast.emit('recimg', image);
+   });
+
+
+
+
+
+
+
+});
+
 
 
 
